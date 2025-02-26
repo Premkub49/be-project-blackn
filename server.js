25a08d4ff77b0991ad8ffdaf59f6a77c24598e9f
ThawinXD <bto2168@gmail.com>
2025-02-26 13:58:05 +0700

    Add API Security

--- server.js
+++ server.js
@@ -5,12 +5,32 @@ const dentists = require("./routes/dentists");
 const bookings = require("./routes/bookings");
 const auth = require("./routes/auth");
 const connectDB = require("./config/db");
+
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const {xss} = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 500,
+});
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
+
 app.use(`/api/v1/dentists`, dentists);
 app.use("/api/v1/bookings", bookings);
 app.use("/api/v1/auth", auth);
